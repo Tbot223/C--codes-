61e2d0aa8e57abc8276f5a387d16a30e3286cff8
Tbot223 <104832561+Tbot223@users.noreply.github.com>
2024-01-26 23:02:19 +0900

    Create 난수_생성.cpp

---
+++ 난수_생성.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <list>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis(0, 99);
+    int number;
+    cout << "뽑을 난수 개수를 입력하세요 : ";
+    cin >> number;
+    for (int i = 0; i < number; i++) {
+        cout << "난수 : " << dis(gen) << std::endl;
+    }
+}
+
